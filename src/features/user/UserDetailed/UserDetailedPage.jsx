42fbe55f0917e0dffed55d36978f9245fe13ff88
Sudarman <oliyud85@gmail.com>
2019-10-29 16:40:30 +0800

    Bagian akhir bab 5

---
+++ src/features/user/UserDetailed/UserDetailedPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserDetailedPage = () => {
+  return (
+    <div>
+      <h1>User detailed page works...</h1>
+    </div>
+  );
+};
+
+export default UserDetailedPage;
