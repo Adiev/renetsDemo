2eaa22301756e93d8433f829595131eda96c7325
Sudarman <oliyud85@gmail.com>
2019-10-31 00:43:42 +0800

    Akhir Bab 8

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -2,10 +2,11 @@
 import React, { Component } from 'react';
 import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
+import { Link } from 'react-router-dom';
 
 class EventListItem extends Component {
   render() {
-    const { event, selectEvent, deleteEvent } = this.props;
+    const { event, deleteEvent } = this.props;
     return (
       <Segment.Group>
         <Segment>
@@ -43,8 +44,8 @@ class EventListItem extends Component {
             content='Hapus'
           />
           <Button
-            onClick={() => selectEvent(event)}
-            as='a'
+            as={Link}
+            to={`/events/${event.id}`}
             color='teal'
             floated='right'
             content='Tampilkan'
